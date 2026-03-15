76c47b1ff45c5c582a03a9629d46c4d5f242c227
zLxx7z <145941506+zLxx7z@users.noreply.github.com>
2026-03-14 23:53:50 -0300

    Create descriptar.py

---
+++ descriptar.py
@@ -0,0 +1,19 @@
+import os
+import pyaes
+
+
+file_name = 'teste.txt.ransomwaretroll'
+file = open(file_name, 'rb')
+file_data = file.read()
+file.close()
+
+key = b'testeransomwares'
+aes = pyaes.AESModeOfOperationCTR(key)
+decrypt_data = aes.decrypt(file_data)
+
+os.remove(file_name)
+
+new_file = 'teste.txt'
+new_file = open(f'{new_file}', 'wb')
+new_file.write(decrypt_data)
+new_file.close()
